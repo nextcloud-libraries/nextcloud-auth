dbb789b5fa11f9808177e7f778f71fb53a23a46a
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-21 15:50:26 +0200

    fix: Resolved linter issues
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,33 +3,33 @@ import typescript from '@rollup/plugin-typescript'
 const external = ['@nextcloud/event-bus']
 
 export default [
-  {
-    input: './lib/index.ts',
-    external,
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json',
-        compilerOptions: { target: 'es5' },
-      }),
-    ],
-    output: [
-      {
-        dir: 'dist',
-        format: 'cjs',
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: 'lib/index.ts',
-    external,
-    plugins: [typescript()],
-    output: [
-      {
-        file: 'dist/index.es.mjs',
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-  },
+	{
+		input: './lib/index.ts',
+		external,
+		plugins: [
+			typescript({
+				tsconfig: './tsconfig.json',
+				compilerOptions: { target: 'es5' },
+			}),
+		],
+		output: [
+			{
+				dir: 'dist',
+				format: 'cjs',
+				sourcemap: true,
+			},
+		],
+	},
+	{
+		input: 'lib/index.ts',
+		external,
+		plugins: [typescript()],
+		output: [
+			{
+				file: 'dist/index.es.mjs',
+				format: 'esm',
+				sourcemap: true,
+			},
+		],
+	},
 ]
